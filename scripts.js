2ee07548f02a9d29441ec55d0594fac8bbbd6447
MathLxz <mathluz.mrx@gmail.com>
2023-02-09 13:56:07 -0300

    Validação de login simples

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 loginBox.addEventListener('load', () => {
     loginBox.classList.toggle('loginBoxAnimation')
 })
+
+function logar(){
+    let email = document.querySelector('#email').value
+    let password = document.querySelector('#password').value 
+
+    if(email == 'admin' && password == 'admin'){
+        alert('Logado!')
+    } else{
+        alert('Preencha os campos para fazer login')
+    }
+}
